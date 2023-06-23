a16028ad4d4b8b74f48445c5016f2f4368d500c2
shilpishah <shilpishah2003@gmail.com>
2023-06-23 12:38:08 -0400

    Update model_preprocessed.py

--- deeplearning_tensorflow/model_preprocessed.py
+++ deeplearning_tensorflow/model_preprocessed.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
-from tensorflow.keras import layers
+from tensorflow import keras
+from keras import layers
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2 as cv
